0c328c7c04d830751476931be65f91eb1b329577
Anton Pershin <tony.pershin@gmail.com>
2020-05-07 20:53:21 +0300

    Allowed for light-weight versions of laminar flow objects without the solution itself (only dissipation rate and KE)

--- restools/laminar_flows.py
+++ restools/laminar_flows.py
@@ -19,12 +19,15 @@ class PlaneCouetteFlow:
     ke
       Laminar kinetic energy
     """
-    def __init__(self, re: float, y: np.ndarray):
+    def __init__(self, re: float, y: np.ndarray = None):
         self.re = re
-        space = Space([y])
-        space.set_elements_names(['y'])
-        self.solution = Field([space.y], space)
-        self.solution.set_elements_names(['u'])
+        if y is not None:
+            space = Space([y])
+            space.set_elements_names(['y'])
+            self.solution = Field([space.y], space)
+            self.solution.set_elements_names(['u'])
+        else:
+            self.solution = None
         self.dissipation_rate = 1./re
         self.ke = 1./6
 
@@ -50,21 +53,24 @@ class PlaneCouetteFlowWithInPhaseSpanwiseOscillations:
     dissipation rate
       Laminar dissipation rate
     """
-    def __init__(self, re: float, a: float, omega: float, t: np.ndarray, y: np.ndarray):
+    def __init__(self, re: float, a: float, omega: float, t: np.ndarray = None, y: np.ndarray = None):
         self.re = re
         self.a = a
         self.omega = omega
         big_omega = np.sqrt(omega * re / 2.)
-        space = Space([t, y])
-        space.set_elements_names(['t', 'y'])
-        t_, y_ = np.meshgrid(space.t, space.y, indexing='ij')
-        big_lambda = np.cos(2.*big_omega) + np.cosh(2.*big_omega)
-        y_plus = big_omega*(1. + y_)
-        y_minus = big_omega*(1. - y_)
-        f = (np.cosh(y_plus)*np.cos(y_minus) + np.cosh(y_minus)*np.cos(y_plus)) / big_lambda
-        g = -(np.sinh(y_plus)*np.sin(y_minus) + np.sinh(y_minus)*np.sin(y_plus)) / big_lambda
-        self.solution = Field([y_, a*(f*np.sin(omega*t_) + g*np.cos(omega*t_))], space)
-        self.solution.set_elements_names(['u', 'w'])
+        if t is not None and y is not None:
+            space = Space([t, y])
+            space.set_elements_names(['t', 'y'])
+            t_, y_ = np.meshgrid(space.t, space.y, indexing='ij')
+            big_lambda = np.cos(2.*big_omega) + np.cosh(2.*big_omega)
+            y_plus = big_omega*(1. + y_)
+            y_minus = big_omega*(1. - y_)
+            f = (np.cosh(y_plus)*np.cos(y_minus) + np.cosh(y_minus)*np.cos(y_plus)) / big_lambda
+            g = -(np.sinh(y_plus)*np.sin(y_minus) + np.sinh(y_minus)*np.sin(y_plus)) / big_lambda
+            self.solution = Field([y_, a*(f*np.sin(omega*t_) + g*np.cos(omega*t_))], space)
+            self.solution.set_elements_names(['u', 'w'])
+        else:
+            self.solution = None
         self.dissipation_rate = 1./re * (1. + a**2 * big_omega / 2. * (np.sinh(2.*big_omega) - np.sin(2.*big_omega)) /
                                          (np.cosh(2.*big_omega) + np.cos(2.*big_omega)))
 
@@ -87,13 +93,16 @@ class StokesBoundaryLayer:
       Field object with components u (streamwise velocity), w (spanwise velocity) and space t (time), y (wall-normal
       coordinate)
     """
-    def __init__(self, re: float, a: float, omega: float, t: np.ndarray, y: np.ndarray):
+    def __init__(self, re: float, a: float, omega: float, t: np.ndarray = None, y: np.ndarray = None):
         self.re = re
         self.a = a
         self.omega = omega
         big_omega = np.sqrt(omega * re / 2.)
-        space = Space([t, y])
-        space.set_elements_names(['t', 'y'])
-        t_, y_ = np.meshgrid(space.t, space.y, indexing='ij')
-        self.solution = Field([y_, a * np.exp(-big_omega*y_) * np.sin(omega*t_ - big_omega*y_)], space)
-        self.solution.set_elements_names(['u', 'w'])
+        if t is not None and y is not None:
+            space = Space([t, y])
+            space.set_elements_names(['t', 'y'])
+            t_, y_ = np.meshgrid(space.t, space.y, indexing='ij')
+            self.solution = Field([y_, a * np.exp(-big_omega*y_) * np.sin(omega*t_ - big_omega*y_)], space)
+            self.solution.set_elements_names(['u', 'w'])
+        else:
+            self.solution = None
