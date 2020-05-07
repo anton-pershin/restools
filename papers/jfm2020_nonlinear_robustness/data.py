463962a252f866426c8cd181d32ac282adf8549f
Anton Pershin <tony.pershin@gmail.com>
2020-05-07 20:47:58 +0300

    Update data model and views for a future paper. Added calculation of dissipation rates and averaged velocity profiles

--- papers/jfm2020_nonlinear_robustness/data.py
+++ papers/jfm2020_nonlinear_robustness/data.py
@@ -3,13 +3,16 @@ from typing import List
 from jsons import JsonSerializable
 
 
-class EdgeStatesInfo:
+class SimulationsInfo:
     """
-    Class EdgeStatesInfo represents a summary data from the campaign of edge tracking runs for different amplitudes and
+    Class SimulationsInfo represents a summary data from the campaign of simulations for different amplitudes and
     frequencies of in-phase spanwise wall oscillations:
 
     res_id
       Research ID associated with the tasks containing the relevant simulations
+
+    re
+      Reynolds number
 
     amplitudes
       A list of amplitudes
@@ -25,9 +28,10 @@ class EdgeStatesInfo:
     task_for_uncontrolled_case
       A task for the uncontrolled case
     """
-    def __init__(self, res_id: str, amplitudes: List[float], frequencies: List[float], tasks: List[List[int]],
+    def __init__(self, res_id: str, re: float, amplitudes: List[float], frequencies: List[float], tasks: List[List[int]],
                  task_for_uncontrolled_case: int):
         self.res_id = res_id
+        self.re = re
         self.amplitudes = amplitudes
         self.frequencies = frequencies
         self.tasks = tasks
@@ -38,9 +42,13 @@ class Summary(JsonSerializable):
     """
     Class Summary is a json-serializable summary of the study.
 
-    edge_states_info
-      EdgeStatesInfo object
+    edge_tracking_simulations
+      SimulationsInfo object with edge tracking simulations
+
+    simulations_with_full_fields_saved
+      SimulationsInfo object with simulations where full flow fields were saved with small time intervals (dT = 1 or 10)
     """
 
-    def __init__(self, edge_states_info: EdgeStatesInfo):
+    def __init__(self, edge_states_info: SimulationsInfo, simulations_with_full_fields_saved: SimulationsInfo):
         self.edge_states_info = edge_states_info
+        self.simulations_with_full_fields_saved = simulations_with_full_fields_saved
