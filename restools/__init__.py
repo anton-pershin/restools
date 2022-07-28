9f59652ef812b9d1fc64ba6391639abe0098e5ee
Anton Pershin <tony.pershin@gmail.com>
2022-07-28 11:51:20 +0300

    Removed old artifacts from __init__

--- restools/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import os.path
-import sys
-
-restools_root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.join(restools_root_path, 'thequickmath'))
-sys.path.append(os.path.join(restools_root_path, 'pycomsdk'))
-sys.path.append(os.path.join(restools_root_path, 'reducedmodels'))
